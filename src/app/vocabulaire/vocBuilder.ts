d51cd2bc427f088cf94462d429c3b1378b90f85c
plrigaux <491172+plrigaux@users.noreply.github.com>
2022-04-07 09:35:53 -0400

    theme 5 semaine 1

--- src/app/vocabulaire/vocBuilder.ts
+++ src/app/vocabulaire/vocBuilder.ts
@@ -1,11 +1,12 @@
 import vocabulaire from '../../resources/vocabulaire.json';
 import stValentin from '../../resources/themes1.json';
 import theme4 from '../../resources/vocabulaire_th4.json';
+import theme5 from '../../resources/vocabulaire_th5.json';
 import { Theme } from './vocabulaireInterfaces';
  
 const themes: Theme[] = vocabulaire
 
-themes.push( ...theme4,...stValentin)
+themes.push( ...theme4, ...theme5, ...stValentin)
 
 export default themes
 
