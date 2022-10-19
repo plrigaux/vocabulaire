9614a84abf76aa28444f16b45a86a3a64ee73c48
plrigaux <491172+plrigaux@users.noreply.github.com>
2022-10-18 22:23:10 -0400

    Theme semaine connection

--- src/app/cardcontrol/cardcontrol.component.ts
+++ src/app/cardcontrol/cardcontrol.component.ts
@@ -1,9 +1,16 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { Theme, Semaine, Groupe, Mot } from '../vocabulaire/vocabulaireInterfaces';
-import { CardComponent } from '../card/card.component';
+import { Component, OnInit, ViewChild } from '@angular/core'
+import {
+  Theme,
+  Semaine,
+  Groupe,
+  Mot
+} from '../vocabulaire/vocabulaireInterfaces'
+import { CardComponent } from '../card/card.component'
 
-import { MatSlideToggleChange } from '@angular/material/slide-toggle';
-import themes from '../vocabulaire/vocBuilder';
+import { MatSlideToggleChange } from '@angular/material/slide-toggle'
+import themes from '../vocabulaire/vocBuilder'
+import { AppConfig, DEFAULT_CONFIG, ThemeSemaine } from '../app-config'
+import { ThemeSetterService } from '../theme-setter.service'
 
 @Component({
   selector: 'app-cardcontrol',
@@ -11,74 +18,83 @@ import themes from '../vocabulaire/vocBuilder';
   styleUrls: ['./cardcontrol.component.scss']
 })
 export class CardcontrolComponent implements OnInit {
-
-  theme: Theme | null = null;
-  semaine: Semaine | null = null;
+  theme: Theme | null = null
+  semaine: Semaine | null = null
   themes: Theme[] = themes
-  groupe: Groupe | null = null;
-  mot: Mot | null = null;
+  groupe: Groupe | null = null
+  mot: Mot | null = null
   mots: Mot[] = []
   motIndex: number = 0
   private _isWriting = false
-  shuffle = false;
-  article: "unune" | "lela" = "unune"
+  shuffle = false
+  article: 'unune' | 'lela' = 'unune'
 
   private _prevDisabled: boolean = true
   private _nextDisabled: boolean = true
+  private app_config: AppConfig = DEFAULT_CONFIG
 
   @ViewChild(CardComponent)
-  private cardComponent!: CardComponent;
-  constructor() { }
+  private cardComponent!: CardComponent
 
-  ngOnInit(): void {
+  private themes_map: Map<number | string, Theme> = new Map()
+  private themes_semaine_map: Map<string, Semaine> = new Map()
+  constructor (private configSrv: ThemeSetterService) {}
 
+  ngOnInit (): void {
+    console.log('THS', themes, themes.length, typeof themes, themes.values())
+
+    this.configSrv.subscribe({
+      next: (config: AppConfig) => {
+        this.app_config = config
+      }
+    })
   }
 
-  getSemaines(): Semaine[] {
+  getSemaines (): Semaine[] {
     //let theme = this.themes.find( (t : Theme) => t.theme == this.theme.theme)
 
     if (this.theme) {
-      return this.theme.semaines ?? [];
+      return this.theme.semaines ?? []
     }
 
-    return [];
+    return []
   }
 
-  shuffleArray(array: any) {
+  shuffleArray (array: any) {
     if (!this.shuffle) {
-      return;
+      return
     }
 
     for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
+      const j = Math.floor(Math.random() * (i + 1))
+      ;[array[i], array[j]] = [array[j], array[i]]
     }
   }
 
-  crunchMot(m: Mot, indice: string) {
+  crunchMot (m: Mot, indice: string) {
     m.indice = indice
     if (Array.isArray(m.classe)) {
       if (m.classe.length >= 1) {
         let cls = m.classe[0]
-        let isNom = cls == "NM" || cls == "NF"
+        let isNom = cls == 'NM' || cls == 'NF'
         if (isNom && m.fem) {
           let newMot: Mot = {
             mot: m.fem,
-            classe: "NF",
+            classe: 'NF',
             indice: indice
           }
           this.mots.push(newMot)
         }
       }
-    } else if (m.classe == "ADJ") {
+    } else if (m.classe == 'ADJ') {
       if (m.fem) {
         let newMot: Mot = {
           mot: m.fem,
-          classe: "ADJ",
+          classe: 'ADJ',
           indice: indice,
-          genre: "FEM"
+          genre: 'FEM'
         }
-        m.genre = "MAS"
+        m.genre = 'MAS'
         this.mots.push(m)
         m = newMot
       }
@@ -87,64 +103,107 @@ export class CardcontrolComponent implements OnInit {
     this.mots.push(m)
   }
 
-  onChangeTheme(theme: Theme) {
-    this.theme = theme
-    if (theme.mots) {
-      this.semaine = null
-      this.mots = []
-      this.motIndex = -1;
-      theme.mots.forEach(m => { this.crunchMot(m, "") });
+  private fillMap () {
+    if (this.themes_map.size > 0) {
+      return
+    }
 
-      this.shuffleArray(this.mots)
-      this.next();
+    for (let t of themes) {
+      const t_id: number | string = t.theme
+      this.themes_map.set(t_id, t)
+      if (t.semaines) {
+        const semaines = t.semaines as Semaine[]
+        for (let s of semaines) {
+            this.themes_semaine_map.set(`${t_id}_${s.semaine}`, s)
+        }
+      }
     }
   }
 
-  onChangeSemaine(semaine: Semaine) {
+  onChangeTheme (theme_id: number | string) {
+    this.fillMap()
+    const theme_obj = this.themes_map.get(theme_id)
+
+    console.log(theme_id, theme_obj, this.themes_map)
+    if (!theme_obj) {
+      return
+    }
+
+    if (this.theme?.theme != theme_obj.theme) {
+      this.semaine = null
+    }
+
+    this.theme = theme_obj
+
+    if (this.theme.mots) {
+      this.semaine = null
+      this.mots = []
+      this.motIndex = -1
+      this.theme.mots.forEach(m => {
+        this.crunchMot(m, '')
+      })
+
+      this.shuffleArray(this.mots)
+      this.next()
+    }
+  }
+
+  onChangeSemaine (semaine_id: number) {
     //onsole.log(semaine)
-    this.semaine = semaine
+
+    if (!this.theme) {
+      return
+    }
+   
+
+    let sem = this.themes_semaine_map.get(`${this.theme.theme}_${semaine_id}`)
+    console.warn("Sem", semaine_id, sem)
+    if (!sem) {
+      console.warn("Sem th", this.themes_semaine_map)
+      return
+    }
+    this.semaine = sem
 
     this.mots = []
-    this.motIndex = -1;
+    this.motIndex = -1
     this.semaine.groupes.forEach((group: Groupe) => {
       let indice = group.indice
       group.mots.forEach(mot => {
         this.crunchMot(mot, indice)
-      });
+      })
     })
-    this.next();
+    this.next()
   }
 
-  onChangeToggle(event: MatSlideToggleChange) {
+  onChangeToggle (event: MatSlideToggleChange) {
     console.log(event)
 
     if (this.semaine) {
-      this.onChangeSemaine(this.semaine)
+      this.onChangeSemaine(this.semaine.semaine)
       return
     }
 
     if (this.theme?.mots) {
-      this.onChangeTheme(this.theme)
+      this.onChangeTheme(this.theme.theme)
       return
     }
   }
 
-  next() {
-    console.log("next - " + this.motIndex)
+  next () {
+    console.log('next - ' + this.motIndex)
     let index = this.motIndex + 1
     this.setMot(index)
-    console.log("next - " + this.motIndex)
+    console.log('next - ' + this.motIndex)
   }
 
-  previous() {
+  previous () {
     let index = this.motIndex - 1
     this.setMot(index)
   }
 
-  private setMot(index: number) {
+  private setMot (index: number) {
     //console.log("setMot - index " + index)
     if (index >= 0 && index < this.mots.length) {
-
       this.motIndex = index
       this.mot = this.mots[this.motIndex]
       //console.log("setMot - new mot " + this.motIndex + " mot " + this.mot.mot)
@@ -165,12 +224,12 @@ export class CardcontrolComponent implements OnInit {
     //console.log(this)
   }
 
-  checkWritingState(isWriting: boolean) {
+  checkWritingState (isWriting: boolean) {
     console.log(`isWriting ${isWriting}`)
     this._isWriting = isWriting
   }
 
-  prevDisabled(): boolean {
+  prevDisabled (): boolean {
     if (this._isWriting) {
       return true
     }
@@ -178,7 +237,7 @@ export class CardcontrolComponent implements OnInit {
     return this._prevDisabled
   }
 
-  nextDisabled(): boolean {
+  nextDisabled (): boolean {
     if (this._isWriting) {
       return true
     }
--- src/app/vocabulaire/vocBuilder.ts
+++ src/app/vocabulaire/vocBuilder.ts
@@ -8,24 +8,43 @@ import JSON5 from 'json5'
 //const themes: Theme[] = vocabulaire
 const themes: Theme[] = []
 
-const getVocabularyAsset = (file: string) => {
-  fetch(file)
+const getVocabularyAsset = (file: string): Promise<any> => {
+  return fetch(file)
     .then(response => response.text())
     .then(text => {
       // Do something with your data
       //console.log(text)
-      let data = JSON5.parse(text)
+      let data : any = JSON5.parse(text)
       //console.log(data)
-      themes.push(...data)
-    })
+      return data
+    }).catch(console.error);
 }
 
 //JSON5.parse(text2)
 
 //themes.push(...theme4, ...theme5, ...stValentin)
+Promise.all([
+  getVocabularyAsset('./assets/voc4_th1.json5'),
+  getVocabularyAsset('./assets/voc4_th2.json5'),
+  getVocabularyAsset('./assets/voc4_th3.json5')
+]).then(data => {
 
-getVocabularyAsset('./assets/voc4_th1.json5')
-getVocabularyAsset('./assets/voc4_th2.json5')
-getVocabularyAsset('./assets/voc4_th3.json5')
+  data.forEach(inData => {
+
+    if (Array.isArray(inData)) {
+
+      inData.forEach(ininData => {
+        themes.push(ininData)
+      })
+    
+    } else {
+      themes.push(inData)
+    }
+
+  })
+  
+  console.log(themes)
+
+})
 
 export default themes
