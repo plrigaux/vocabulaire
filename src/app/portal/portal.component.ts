73d3576c82044dd57fd2c20f78d583d7fe1a86dc
plrigaux <491172+plrigaux@users.noreply.github.com>
2022-10-27 11:05:54 -0400

    selector from fron page

--- src/app/portal/portal.component.ts
+++ src/app/portal/portal.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnDestroy, OnInit } from '@angular/core'
 import { MatRadioChange } from '@angular/material/radio'
 import { Subscription } from 'rxjs'
+import { AppConfig, DEFAULT_CONFIG } from '../app-config'
 import { ThemeSetterService } from '../theme-setter.service'
+import { VocabulaireDataHandlerService } from '../vocabulaire/vocabulaire-data-handler.service'
+import { Theme } from '../vocabulaire/vocabulaireInterfaces'
 
 @Component({
   selector: 'app-portal',
@@ -9,7 +12,34 @@ import { ThemeSetterService } from '../theme-setter.service'
   styleUrls: ['./portal.component.scss']
 })
 export class PortalComponent implements OnInit {
-  constructor () {}
+  constructor (
+    private configSrv: ThemeSetterService,
+    private vocSrv: VocabulaireDataHandlerService
+  ) {}
 
-  ngOnInit (): void {}
+  get voc_themes (): Theme[] {
+    return this.vocSrv.getThemes()
+  }
+
+  private myEventSubscriptions: Subscription[] = []
+  app_config: AppConfig = { ...DEFAULT_CONFIG }
+
+  ngOnInit (): void {
+    this.myEventSubscriptions.push(
+      this.configSrv.subscribe({
+        next: (config: AppConfig) => {
+          this.app_config = config
+         }
+      })
+    )
+  }
+
+
+  practice(theme_id : number | string, semaine_id : number) {
+
+    this.app_config.theme_semaine.theme = Number(theme_id)
+    this.app_config.theme_semaine.semaine = semaine_id
+
+    this.configSrv.next(this.app_config)
+  }
 }
